b5519629dd25cfe135be2bdf9731302b7ddeeb1b
lonelygoogle <lonelygoogle@qq.com>
2020-04-25 21:55:42 +0800

    fulfill session

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+/*
+ * @Description: 
+ * @Author:  
+ * @Date: 2020-04-14 17:53:45
+ * @LastEditTime: 2020-04-25 21:46:31
+ * @LastEditors:  
+ */
 const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 
+// session 数据
+const SESSION_DATA = {}
 // 用于处理post data
 const getPostData = (req) => {
     const promise = new Promise((resolve,reject) => {
@@ -34,6 +43,33 @@ const serverHandle = (req,res)=> {
     //解析query
     req.query = querystring.parse(url.split('?')[1])
 
+    //解析cookie
+    req.cookie = {}
+    const cookieStr = req.headers.cookie || ''
+    cookieStr.split(';').forEach(item => {
+        if (!item) {
+            return
+        }
+        const arr = item.split('=')
+        const key = arr[0]
+        const value = arr[1]
+        req.cookie[key] = value
+        console.log(key, value)
+    });
+    //解析session
+    let needSetCookie = false
+    let userId = req.cookie.userId
+    if (userId) {
+        if (!SESSION_DATA[userId]) {
+            SESSION_DATA[userId] = {}
+        } 
+    } else {
+        needSetCookie = true
+        userId = `${Date.now()}_${Math.random()}`
+        SESSION_DATA[userId] = {}
+    }
+    req.session = SESSION_DATA[userId]
+
     //处理postData
     getPostData(req).then(postData => {
         req.body = postData
@@ -42,6 +78,9 @@ const serverHandle = (req,res)=> {
         const blogResult = handleBlogRouter(req,res)
         if (blogResult) {
             blogResult.then((blogData) => {
+                if (needSetCookie) {
+                    res.setHeader('Set-Cookie',`userId=${userId}; path='/'; httpOnly`)
+                }
                 res.end(JSON.stringify(blogData))
             })
             return
@@ -49,14 +88,12 @@ const serverHandle = (req,res)=> {
 
         // 处理user路由
 
-        // const userData = handleUserRouter(req,res)
-        // if (userData) {
-        //     res.end(JSON.stringify(userData))
-        //     return
-        // }
         const userResult = handleUserRouter(req,res)
         if (userResult) {
             userResult.then((userData) =>{
+                if (needSetCookie) {
+                    res.setHeader('Set-Cookie',`userId=${userId}; path='/'; httpOnly`)
+                }
                 res.end(JSON.stringify(userData))
             })
             return
--- src/router/user.js
+++ src/router/user.js
@@ -1,4 +1,12 @@
-const { loginCheck } = require('../controller/user')
+/*
+ * @Description: 
+ * @Author:  
+ * @Date: 2020-04-14 17:53:45
+ * @LastEditTime: 2020-04-25 21:47:51
+ * @LastEditors:  
+ */
+
+const { login } = require('../controller/user')
 const { SuccessModel, ErrorModel } =require('../model/resModel')
 
 const handleUserRouter = (req, res) => {
@@ -6,17 +14,36 @@ const handleUserRouter = (req, res) => {
 
 
     // 登录 
-    if (method === 'POST' && req.path === '/api/user/login') {
-        const { username, password } = req.body
-        const result = loginCheck(username, password)
+    if (method === 'GET' && req.path === '/api/user/login') {
+        // const { username, password } = req.body
+        const { username, password } = req.query
+        const result = login(username, password)
         return result.then(data => {
             if (data.username) {
+                // 设置session
+                req.session.username = data.username
+                req.session.realname = data.realname
+                console.log('req.session is', req.session)
+                console.log(Math.random())
                 return new SuccessModel()
             } else {
                 return new ErrorModel('登陆失败')
             }
         })
     }
+
+    //登录验证
+    if (method === 'GET' && req.path === '/api/user/login-test') {
+        if (req.session.username) {
+            return Promise.resolve(new SuccessModel({
+                session : req.session
+            }
+            )) 
+        } else {
+            return Promise.resolve(new ErrorModel('尚未登录')) 
+        }
+    }
 }
+
 
 module.exports = handleUserRouter
